2db9bd5920c580cbaa28e31a2681dfb675b5895e
Selamsew Dagne <selamelsadagne@gmail.com>
2026-09-14 16:46:09 +0300

    complete the crud operation with database

--- main.go
+++ main.go
@@ -1,96 +1,163 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Todo struct {
-	ID        int    `json:"id"`
-	Completed bool   `json:"completed"`
-	Body      string `json:"body" `
+	ID        primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Completed bool               `json:"completed"`
+	Body      string             `json:"body" `
 }
 
+var collection *mongo.Collection
+
 func main() {
-	fmt.Println("Hello world")
-	// var name string = "selam"
-	// const secondName = "Abebe"
-
-	// thirdName := "jhon"
-	// fmt.Println(name)
-	// fmt.Println(secondName)
-	// fmt.Println(thirdName)
-
-	//create a new application
-	app := fiber.New()
-
+	//load the .env file
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatal("Error loading the env file")
+		log.Fatal("Error loading .env file")
 	}
+
+	MONGODB_URI := os.Getenv("MONGODB_URI")
 	PORT := os.Getenv("PORT")
-	todos := []Todo{}
 
-	//get todos
-	app.Get("/api/todos", func(c *fiber.Ctx) error {
-		// return c.Status(200).JSON(fiber.Map{"msg": "hello world"})
-		return c.Status(200).JSON(todos)
-	})
+	//connect to the database
+	clientoptions := options.Client().ApplyURI(MONGODB_URI)
 
-	//create a todo
-	app.Post("/api/todos", func(c *fiber.Ctx) error {
-		todo := &Todo{}
-		if err := c.BodyParser(todo); err != nil {
+	client, err := mongo.Connect(context.Background(), clientoptions)
+	if err != nil {
+		log.Fatal("Error: ", err)
+	}
+
+	defer client.Disconnect(context.Background())
+
+	//ping the database
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal("Error: ", err)
+	}
+
+	fmt.Println("Connected to the database")
+
+	collection = client.Database("golang_db").Collection("todos")
+
+	app := fiber.New()
+
+	app.Get("/api/todos", getTodos)
+	app.Get("/api/todos/:id", getSignleTodo)
+	app.Post("/api/todos", createTodo)
+	app.Patch("/api/todos/:id", updateTodo)
+	app.Delete("/api/todos/:id", deleteTodo)
+
+	log.Fatal(app.Listen(":" + PORT))
+
+}
+
+//create handler functions
+
+func getTodos(c *fiber.Ctx) error {
+	//array | slice
+	var todos []Todo
+	//no filter
+	cursor, err := collection.Find(context.Background(), bson.M{})
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"Error": "Error geting the todos"})
+	}
+	//close the cursor after the function returns
+	defer cursor.Close(context.Background())
+	//map through it
+	for cursor.Next(context.Background()) {
+		var todo Todo
+		if err := cursor.Decode(&todo); err != nil {
 			return err
 		}
+		todos = append(todos, todo)
+	}
 
-		if todo.Body == "" {
-			return c.Status(400).JSON(fiber.Map{"error": "Todo body requierd"})
-		}
-		todo.ID = len(todos) + 1
-		todos = append(todos, *todo)
-
-		//pointer
-		// var x int = 5   // 0x7ffe445d8b14
-		// var p *int = &x //0x7ffe445d8b14
-		// fmt.Println(p)  // 0x7ffe445d8b14
-		// fmt.Println(*p) // 5
-
-		return c.Status(201).JSON(todo)
+	return c.JSON(fiber.Map{
+		"message": "data fetched successfully",
+		"data":    todos,
 	})
+}
 
-	//updating todos
+func getSignleTodo(c *fiber.Ctx) error {
+	var todo Todo
+	id := c.Params("id")
+	objectId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return c.Status(404).JSON(fiber.Map{"Error": "Invalid Id"})
+	}
+	err = collection.FindOne(context.Background(), bson.M{"_id": objectId}).Decode(&todo)
 
-	app.Patch("/api/todos/:id", func(c *fiber.Ctx) error {
-		id := c.Params("id")
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"Error": "Todo not found"})
+	}
+	return c.Status(200).JSON(todo)
+}
 
-		for i, todo := range todos {
-			if fmt.Sprint(todo.ID) == id {
-				todos[i].Completed = true
-				return c.Status(200).JSON(todos[i])
-			}
-		}
+func createTodo(c *fiber.Ctx) error {
+	// pointer
+	todo := new(Todo)
 
-		return c.Status(404).JSON(fiber.Map{"error": "Todo not found"})
+	if err := c.BodyParser(todo); err != nil {
+		return err
+	}
+
+	if todo.Body == "" {
+		return c.Status(400).JSON(fiber.Map{"Msg": "Todo body can not be empty"})
+	}
+	insertResult, err := collection.InsertOne(context.Background(), todo)
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"Error": "Error creaing a todo"})
+	}
+	todo.ID = insertResult.InsertedID.(primitive.ObjectID)
+	return c.Status(201).JSON(todo)
+}
+
+func updateTodo(c *fiber.Ctx) error {
+	id := c.Params("id")
+	objectId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return c.Status(404).JSON(fiber.Map{"Msg": "Invalid Id"})
+	}
+
+	filter := bson.M{"_id": objectId}
+	update := bson.M{"$set": bson.M{"completed": true}}
+	//_ -I don't care about this value.
+	_, err = collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		return err
+	}
+	return c.Status(200).JSON(fiber.Map{
+		"message": "todo updated successfully",
 	})
+}
 
-	app.Delete("/api/todos/:id", func(c *fiber.Ctx) error {
-		id := c.Params("id")
+func deleteTodo(c *fiber.Ctx) error {
+	id := c.Params("id")
+	objectId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return c.Status(404).JSON(fiber.Map{"Error": "Invalid id"})
+	}
 
-		for i, todo := range todos {
-			if fmt.Sprint(todo.ID) == id {
-				todos = append(todos[:i], todos[i+1:]...)
-				return c.Status(200).JSON(fiber.Map{"msg": "Todo deleted sucessfully"})
-			}
-		}
+	filter := bson.M{"_id": objectId}
 
-		return c.Status(404).JSON(fiber.Map{"error": "Todo not found!"})
+	_, err = collection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"Error": "Something went wrong"})
+	}
 
-	})
-
-	log.Fatal(app.Listen(PORT))
+	return c.Status(200).JSON(fiber.Map{"Msg": "todo deleted successfully!"})
 }
